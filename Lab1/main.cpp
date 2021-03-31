bb20c1f1b2a806080c5f696848f63faf1fa5665a
李泳昊 <68105403+setsuna-Lzy@users.noreply.github.com>
2021-04-01 00:03:37 +0800

    lyh

--- Lab1/main.cpp
+++ Lab1/main.cpp
@@ -14,6 +14,12 @@ using namespace  std;
 #define B 10
 mutex __mutex;
 mutex __mutex2;
+// mutex _input;
+// condition_variable inputEmpty;
+string questionBuffer[1000];//问题缓存，最高支持1000个问题输入
+int questionIn=0;//指针表示有效问题个数，从0开始
+int questionLocal=0;//当前处理的文件指针
+
 bool buffer[B]={false};//标志位
 int AnsBuffer[B][81]={0};//输出缓存，大小肯定要大于线程个数
 long long posi=0;
@@ -64,7 +70,7 @@ void* LHZFUN(void* arg){
         for(int i=0;i<N;i++) {
             boardW[i]=p->Tboard[i];
         }
-        //putAns(p->num,boardW);
+        putAns(p->num,boardW);
 //        if(!solved()){
 //            printf("work wrong!\n");
 //            assert(0);
@@ -81,52 +87,76 @@ void* LHZFUN(void* arg){
 void* getinput(void* arg){
     //锁suspend
     string fname;
-    while(scanf("%s",&fname)){
-        fpT = freopen("D:/ProgramFiles (x86)/JetBrains/Code/test/test1000","r",stdin);
+    while(cin>>fname){
+        // fpT = freopen(fname,"r",stdin);
+        questionBuffer[questionIn]=fname;
+        questionIn++;
+        cout<<questionLocal<<" "<<questionIn<<"\n";
     }
+    
+    pthread_exit(NULL);
 }
 
 void creatInput(){
-    if(!pthread_create(&inputT,NULL,getinput,NULL)){
+    if(pthread_create(&inputT,NULL,getinput,NULL)){
         printf("create thread wrong!\n");
     }
 }
 
 
 
+
+
+
+
 int main(int argc, char* argv[])
 {
-    FILE* fp = fopen(argv[1], "r");
+    // FILE* fp = fopen(argv[1], "r");
 //    FILE* fp = freopen("D:/ProgramFiles (x86)/JetBrains/Code/test/test1000", "r",stdin);
-    if(fp==NULL) printf("wrong! get file fail.\n");
+    // if(fp==NULL) printf("wrong! get file fail.\n");
+    creatInput();
     char puzzle[128];
     int total_solved = 0;
     long long total = 0;
     ThreadPool p(7);
     p.run();
-    int PT=0;
-    int64_t start = now();
-    while (fgets(puzzle, sizeof puzzle, fp) != NULL) {
-        if (strlen(puzzle) >= N) {
-            PT=(PT+1)%B;
-            __mutex.lock();
-            T[PT].num=total;
-            ++total;
-            input(puzzle);
-            init_cache();
-            T[PT].getT();
-            p.dispatch(LHZFUN, &T[PT]);
-            if(total%B==0){
-                p.sync();//分步读入文件
+    
+    while(1){
+        // while(questionLocal>=questionIn){
+        // }
+        if(questionLocal<questionIn)questionLocal++;
+    /*    FILE* fp = fopen(questionBuffer[questionLocal].c_str(),"r");
+
+
+
+
+        int PT=0;
+        int64_t start = now();
+
+        while (fgets(puzzle, sizeof puzzle, fp) != NULL) {
+            if (strlen(puzzle) >= N) {
+                PT=(PT+1)%B;
+                __mutex.lock();
+                T[PT].num=total;
+                ++total;
+                input(puzzle);
+                init_cache();
+                T[PT].getT();
+                p.dispatch(LHZFUN, &T[PT]);
+                if(total%B==0){
+                    p.sync();//分步读入文件
+                }
+                __mutex.unlock();
             }
-            __mutex.unlock();
         }
+
+        p.sync();
+        int64_t end = now();
+        delete T;
+        double sec = (end-start)/1000000.0;
+        printf("%f sec %f ms each %d\n", sec, 1000*sec/total, total_solved);*/
+
     }
-    p.sync();
-    int64_t end = now();
-    delete T;
-    double sec = (end-start)/1000000.0;
-    printf("%f sec %f ms each %d\n", sec, 1000*sec/total, total_solved);
     return 0;
 }
 
